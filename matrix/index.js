eaa0ad3557f42e683ed4b1aa3674bdac3eb23813
aSpired92 <maciejkobiela92@gmail.com>
2024-01-20 13:34:10 +0100

    matrix solution

--- matrix/index.js
+++ matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const result = [];
+
+    for (let i = 0; i < n; i++) {
+        result.push(new Array(n).fill(0));
+    }
+
+    let counter = 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+
+    while (startRow <= endRow && startColumn <= endColumn) {
+        for (let i = startColumn; i <= endColumn; i++) {
+            result[startRow][i] = counter++;
+        }
+        startRow++;
+
+        for (let i = startRow; i <= endRow; i++) {
+            result[i][endColumn] = counter++;
+        }
+        endColumn--;
+
+        if (startRow <= endRow) {
+            for (let i = endColumn; i >= startColumn; i--) {
+                result[endRow][i] = counter++;
+            }
+            endRow--;
+        }
+
+        if (startColumn <= endColumn) {
+            for (let i = endRow; i >= startRow; i--) {
+                result[i][startColumn] = counter++;
+            }
+            startColumn++;
+        }
+    }
+
+    return result;
+}
 
 module.exports = matrix;
