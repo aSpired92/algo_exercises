19a4f41b02ee469de29776b1a1e7ffee5a25826e
aSpired92 <maciejkobiela92@gmail.com>
2024-01-20 13:34:29 +0100

    sorting solution

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,69 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    const len = arr.length;
 
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
 }
 
 function selectionSort(arr) {
+    const len = arr.length;
 
+    for (let i = 0; i < len - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            // Swap elements if needed
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+
+    return arr;
 }
 
 function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
 
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
